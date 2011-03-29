ddbfc145e1914402d7b7ec32012dde006df84044
iMac <iMac@iMac-iMac.local>
2011-03-29 17:31:11 +0200

    Początkowy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Strona::Application.config.secret_token = 'ac342beede1c8d44f97a502de1f8c6cced3f9922642caad557db15503e8efb36ed09dc536c23ebb034361366ad1072353077382982589003bf9a884dc91d1885'
